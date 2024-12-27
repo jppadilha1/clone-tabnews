48c7a2376bd124f9aebe05d798f4eb9a6a24ee7d
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2024-12-27 17:05:14 +0000

    emogi

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-return <h1>Procura a satisfação de veres morrer os teus vícios antes de ti. - Sêneca</h1>
+return <h1>Procura a satisfação de veres morrer os teus vícios antes de ti. - Sêneca 🤠</h1>
 }
 
 export default Home;
