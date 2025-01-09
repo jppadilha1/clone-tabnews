05093083356ad7e67adfcc7ac409214599c33d44
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-01-09 13:53:19 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-return <h1>Procura a satisfação de veres morrer os teus vícios antes de ti. - Sêneca 🤠</h1>
+  return (
+    <h1>
+      Procura a satisfação de veres morrer os teus vícios antes de ti. - Sêneca
+      🤠
+    </h1>
+  );
+}
+
+function teste() {
+  print("Identação erradaaaa");
 }
 
 export default Home;
