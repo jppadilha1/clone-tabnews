8c48102f829c9b7486735a1b63e3843b437adf61
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-03-17 19:13:18 +0000

    try catch any error

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,37 +4,40 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClientInstance = await database.getNewClient();
+  try {
+    const dbClientInstance = await database.getNewClient();
 
-  const defaultMigrationsOptions = {
-    dbClient: dbClientInstance,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+    const defaultMigrationsOptions = {
+      dbClient: dbClientInstance,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClientInstance.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-
-    await dbClientInstance.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      await dbClientInstance.end();
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(migratedMigrations);
-  }
 
-  await dbClientInstance.end();
-  return response.status(405);
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      await dbClientInstance.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+
+    return response.status(405);
+  } catch (e) {
+    console.log(e);
+  }
 }
